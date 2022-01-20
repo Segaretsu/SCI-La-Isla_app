ade3f863249db8c2a6ede6f47fb6460798f65d57
Jhon Sebastian <Sebastamriel@gmail.com>
2022-01-20 16:57:25 -0500

    Home page completed - 20/01/2022, 04:57 p.m.

--- lib/pages/navpages/home_page.dart
+++ lib/pages/navpages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_la_isla/modules/misc/colors.dart';
 import 'package:flutter_la_isla/modules/widgets/app_large_text.dart';
+import 'package:flutter_la_isla/modules/widgets/app_text.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,10 +13,17 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
 
+  var images = {
+    "balloning.png" : "Ventas",
+    "hiking.png" : "Productos",
+    "kayaking.png" : "Categorias",
+    "snorkling.png" : "Medios de pago",
+  };
+
   @override
   Widget build(BuildContext context) {
     TabController _tabController = TabController(length: 3, vsync: this);
-    // TODO: implement build
+
     return Scaffold(
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -39,13 +48,13 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
               ],
             ),
           ),
-          SizedBox(height: 40,),
-          // Opciones text
+          SizedBox(height: 30,),
+          // Estadisticas text
           Container(
             margin: const EdgeInsets.only(left: 20),
             child: AppLargeText(text: "Descubre"),
           ),
-          SizedBox(height: 30,),
+          SizedBox(height: 20,),
           // tabbar
           Container(
             child: Align(
@@ -57,6 +66,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                 unselectedLabelColor: Colors.grey,
                 isScrollable: true,
                 indicatorSize: TabBarIndicatorSize.label,
+                indicator: CircleTabIndicator(color: AppColors.mainColor, radius: 4),
                 tabs: [
                   Tab(text: "Más vendido",),
                   Tab(text: "Por agotarse",),
@@ -66,20 +76,125 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
             ),
           ),
           Container(
+            padding: const EdgeInsets.only(left: 20),
             height: 300,
             width: double.maxFinite,
             child: TabBarView(
               controller: _tabController,
               children: [
-                Text("Hi"),
+                ListView.builder(
+                  itemCount: 3,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      margin: const EdgeInsets.only(right: 15, top: 10),
+                      width: 200,
+                      height: 300,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.white,
+                        image: DecorationImage(
+                          image: AssetImage("images/mountain.jpeg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    );
+                  },
+                ),
                 Text("There"),
                 Text("Bye"),
               ],
             ),
-          )
+          ),
+          SizedBox(height: 30,),
+          Container(
+            margin: const EdgeInsets.only(left: 20, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AppLargeText(text: "Más funcionalidades", size:22,),
+                AppText(text: "Ver todas", color: AppColors.textColor1),
+              ],
+            ),
+          ),
+          SizedBox(height: 10,),
+          Container(
+            height: 120,
+            width: double.maxFinite,
+            margin: const EdgeInsets.only(left: 20),
+            child: ListView.builder(
+              itemCount: 4,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (_, index) {
+                return Container(
+                  margin: const EdgeInsets.only(right: 30),
+                  child: Column(
+                    children: [
+                      Container(
+                          //margin: const EdgeInsets.only(right: 50),
+                          width: 80,
+                          height: 80,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.white,
+                            image: DecorationImage(
+                              image: AssetImage("images/" + images.keys.elementAt(index)),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10,),
+                        Container(
+                          child: AppText(
+                            text: images.values.elementAt(index),
+                            color: AppColors.textColor2
+                          ),
+                        ),
+                    ],
+                  ),
+                );
+            }),
+          ),
         ],
       ),
     );
   }
 
 }
+
+class CircleTabIndicator extends Decoration {
+  final Color color;
+  double radius;
+
+  CircleTabIndicator({
+    required this.color,
+    required this.radius
+  });
+
+  @override
+  BoxPainter createBoxPainter([VoidCallback? onChanged]) {
+    return _CiclePainter(color:color, radius:radius);
+  }
+
+}
+
+class _CiclePainter extends BoxPainter {
+
+  final Color color;
+  double radius;
+  
+  _CiclePainter({
+    required this.color,
+    required this.radius
+  });
+
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
+    Paint _paint = Paint();
+    _paint.color = color;
+    _paint.isAntiAlias = true;
+    final Offset circleOffset = Offset(configuration.size!.width/2 -radius/2, configuration.size!.height -radius);
+    canvas.drawCircle(offset + circleOffset, radius, _paint);
+  }
+    
+  }
